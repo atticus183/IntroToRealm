f3e69f99aed9f2f1ace359a1ff5c7b4e8d7f54d6
Josh R <seventytwoapp@gmail.com>
2020-05-04 12:54:54 -0500

    Instead of commenting out code to ensure test data and relationships weren't duplicated, checks were added.

--- IntroToRealm/ViewController.swift
+++ IntroToRealm/ViewController.swift
@@ -11,6 +11,7 @@ import RealmSwift
 
 class ViewController: UIViewController {
 
+    //Creates a default realm
     let realm = try! Realm()
     
     
@@ -19,11 +20,10 @@ class ViewController: UIViewController {
         
         print(realm.configuration.fileURL ?? "Realm URL not found.")
         
-        //MARK: Create test data
-        //Uncomment if you delete the app from the simulator and need to recreate test data
-        //Comment methods out if objects already exist in the realm
-//        createCustomersAndBranchManager()
-//        createBank()
+        //MARK: Create test data -- these methods check to see if data has already been created
+        createCustomersAndBranchManager()
+        createBank()
+        
         
         //MARK: Retrieve all customers, branch manager, and bank
         let allCustomers = realm.objects(Customer.self)
@@ -38,15 +38,9 @@ class ViewController: UIViewController {
         
         print("Lauren's new last name is \(branchManager.lastName)")
         
-        //MARK: Add relationships
-        //Uncomment if you delete the app from the simulator and need to reestablish the relationships
-//        try! realm.write {
-//            usBank.branchManager = branchManager
-//
-//            for customer in allCustomers {
-//                usBank.customers.append(customer)
-//            }
-//        }
+        
+        //MARK: Add relationships method call
+        createRelationships()
         
         //MARK: Retrieving the customer's bank from the Customer class
         let firstCustomersBank  = allCustomers.first!.bank.first!
@@ -59,34 +53,62 @@ class ViewController: UIViewController {
     }
     
     private func createCustomersAndBranchManager() {
-        try! realm.write {
-            let customer1 = Customer()
-            customer1.firstName = "Jon"
-            customer1.lastName = "Smith"
-            customer1.currentBalance = 1234.12
-            
-            let customer2 = Customer()
-            customer2.firstName = "Susan"
-            customer2.lastName = "Alexander"
-            customer2.currentBalance = 54321.10
-            
-            let branchManager = BranchManager()
-            branchManager.firstName = "Lauren"
-            branchManager.lastName = "Scott"
-            
-            realm.add([customer1, customer2, branchManager])
+        let customers = realm.objects(Customer.self)
+        let branchManagers = realm.objects(BranchManager.self)
+        
+        //Check to see if the test data exists.  If it does, not create additional.
+        if customers.count == 0 && branchManagers.count == 0 {
+            try! realm.write {
+                let customer1 = Customer()
+                customer1.firstName = "Jon"
+                customer1.lastName = "Smith"
+                customer1.currentBalance = 1234.12
+                
+                let customer2 = Customer()
+                customer2.firstName = "Susan"
+                customer2.lastName = "Alexander"
+                customer2.currentBalance = 54321.10
+                
+                let branchManager = BranchManager()
+                branchManager.firstName = "Lauren"
+                branchManager.lastName = "Scott"
+                
+                realm.add([customer1, customer2, branchManager])
+            }
         }
     }
     
     private func createBank() {
-        try! realm.write {
-            let usBank = Bank()
-            usBank.name = "US Bank"
-            usBank.latitude = 44.9778
-            usBank.longitude = 93.2650
-            usBank.type = Bank.BankType.retailBank.rawValue
-            
-            realm.add(usBank)
+        let banks = realm.objects(Bank.self)
+        
+        //Check to see if the test data exists, if not, create one
+        if banks.count == 0 {
+            try! realm.write {
+                let usBank = Bank()
+                usBank.name = "US Bank"
+                usBank.latitude = 44.9778
+                usBank.longitude = 93.2650
+                usBank.type = Bank.BankType.retailBank.rawValue
+                
+                realm.add(usBank)
+            }
+        }
+    }
+    
+    private func createRelationships() {
+        let allCustomers = realm.objects(Customer.self)
+        let branchManager = realm.objects(BranchManager.self).first!
+        let usBank = realm.objects(Bank.self).first!
+        
+        //Check to see if relationships already exists, if not, create them.
+        if usBank.branchManager == nil && usBank.customers.count == 0 {
+            try! realm.write {
+                usBank.branchManager = branchManager
+                
+                for customer in allCustomers {
+                    usBank.customers.append(customer)
+                }
+            }
         }
     }
     
